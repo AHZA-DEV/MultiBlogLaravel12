de1840760e5010942c020ef5fb0b9a266cf48bd2
AHZA-DEV <kentdiyo@gmail.com>
2025-09-13 12:13:20 +0700

    For template laravel12

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,82 @@
+        <nav class="sidebar" id="sidebar">
+            <div class="sidebar-header">
+                <div class="d-flex align-items-center">
+                    <i class="bi bi-speedometer2 text-primary me-2"></i>
+                    <span class="fw-bold">Skydash</span>
+                </div>
+            </div>
+
+            <div class="sidebar-menu">
+                <ul class="list-unstyled">
+                    <li class="menu-item active">
+                        <a href="index.html" class="menu-link" data-title="Dashboard">
+                            <i class="bi bi-house-door"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="layouts.html" class="menu-link" data-title="Layouts">
+                            <i class="bi bi-layout-sidebar"></i>
+                            <span>Layouts</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="widgets.html" class="menu-link" data-title="Widgets">
+                            <i class="bi bi-grid-3x3-gap"></i>
+                            <span>Widgets</span>
+                            <span class="badge bg-success ms-auto">PRO</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="ui-elements.html" class="menu-link" data-title="UI Elements">
+                            <i class="bi bi-ui-radios"></i>
+                            <span>UI Elements</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="advanced-ui.html" class="menu-link" data-title="Advanced UI">
+                            <i class="bi bi-layers"></i>
+                            <span>Advanced UI</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="form-elements.html" class="menu-link" data-title="Form elements">
+                            <i class="bi bi-journal-text"></i>
+                            <span>Form elements</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="editors.html" class="menu-link" data-title="Editors">
+                            <i class="bi bi-pencil-square"></i>
+                            <span>Editors</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="charts.html" class="menu-link" data-title="Charts">
+                            <i class="bi bi-bar-chart"></i>
+                            <span>Charts</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="table.html" class="menu-link" data-title="Tables">
+                            <i class="bi bi-table"></i>
+                            <span>Tables</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="popups.html" class="menu-link" data-title="Popups">
+                            <i class="bi bi-app-indicator"></i>
+                            <span>Popups</span>
+                            <span class="badge bg-success ms-auto">PRO</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="notification.html" class="menu-link" data-title="Notification">
+                            <i class="bi bi-bell"></i>
+                            <span>Notification</span>
+                            <span class="badge bg-success ms-auto">PRO</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
